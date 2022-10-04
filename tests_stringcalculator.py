ee79d3a5a1e210fc53f29f98fe42253286f47982
kaarlaglez <83715502+kaarlaglez@users.noreply.github.com>
2022-10-03 20:10:52 -0500

    test

--- tests_stringcalculator.py
+++ tests_stringcalculator.py
@@ -1,11 +1,12 @@
+from tkinter import NONE
 import unittest
-import stringcalculator import a
+import stringcalculator 
 
 
 class TestStringMethods(unittest.TestCase):
-
     def test_zero_args(self):
         self.assertEqual(stringcalculator.Add(""), 0)
+        self.assertEqual(stringcalculator.Add(None), 0)
 
 
 if __name__ == '__main__':
