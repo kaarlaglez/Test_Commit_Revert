9e073f594d80a735277ac0f73c4d5440a0888717
kaarlaglez <83715502+kaarlaglez@users.noreply.github.com>
2022-10-03 19:43:32 -0500

    progress

--- stringcalculator.py
+++ stringcalculator.py
@@ -1,19 +1,6 @@
-import re
-import pytest
-from calc.calculator import stringcalculator
 
 
-#def Add(a):
+
+def Add(a):
   #Add implementation
-#  return 0
-
-
-def test_stringcalculator_should_return_zero_on_empty_string():
-  result = stringcalculator("")
-  assert result == 0
-  
-
-def test_stringcalculator_should_return_correct_numeric_value_on_one():
-  result = stringcalculator("1")
-  assert result == 1
-  
+  return 0
