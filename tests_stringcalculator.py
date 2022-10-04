86538343bd45fcabbb12be03b9bc08795dcef2a0
kaarlaglez <83715502+kaarlaglez@users.noreply.github.com>
2022-10-03 20:17:30 -0500

    test

--- tests_stringcalculator.py
+++ tests_stringcalculator.py
@@ -7,6 +7,9 @@ class TestStringMethods(unittest.TestCase):
     def test_zero_args(self):
         self.assertEqual(stringcalculator.Add(""), 0)
         self.assertEqual(stringcalculator.Add(None), 0)
+        
+    def test_numbers_in:expression_are_converted:to:integers(self):
+        self.assertEqual(stringcalculator.Add("0"), 0)
 
 
 if __name__ == '__main__':
